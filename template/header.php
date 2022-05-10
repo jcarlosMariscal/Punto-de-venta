87728847e8ff72e0fd75cf62a648d9921595ce78
Karl16Animelos20 <juanc.ramirezm16@gmail.com>
2022-05-09 21:59:03 -0500

    header ready

---
+++ template/header.php
@@ -0,0 +1,28 @@
+<header class="header">
+        <div class="system">
+            <div class="system__info">
+                <div class="info__logo">
+                    <span><i class="fa-solid fa-computer"></i></i></span>
+                </div>
+                <div class="info__text">
+                    <h3 class="info__name">Punto de Venta</h3>
+                    <h5 class="info__welcome">Bienvenido(a) al sistema</h5>
+                </div>
+            </div>
+            <hr>
+            <div class="system__logout">
+                <a href="../" class="logout"><i class=" nav-icon fa-solid fa-arrow-left"></i></span> Salir del Sistema</a>
+            </div>
+        </div>
+        <nav class="navbar">
+            <ul class="navbar__ul">
+                <li class="navbar__li"><a href="" class="ul__link"><i class="nav-icon fa-solid fa-boxes-stacked"></i> Productos/almacen</a></li>
+                <li class="navbar__li"><a href="" class="ul__link"><i class="nav-icon fa-solid fa-hands-holding-circle"></i> Proveedor</a></li>
+                <li class="navbar__li"><a href="" class="ul__link"><i class="nav-icon fa-solid fa-handshake"></i> Compras</a></li>
+                <li class="navbar__li"><a href="" class="ul__link"><i class="nav-icon fa-solid fa-file-invoice-dollar"></i> Ventas</a></li>
+                <li class="navbar__li"><a href="" class="ul__link"><i class="nav-icon fa-solid fa-user-group"></i> Mi personal</a></li>
+                <li class="navbar__li"><a href="" class="ul__link"><i class="nav-icon fa-solid fa-gears"></i> Configuración</a></li>
+                <li class="navbar__li"><a href="" class="ul__link"><i class="nav-icon fa-solid fa-circle-info"></i> Información del Sistema</a></li>
+            </ul>
+        </nav>
+    </header>
---
+++ view/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@100;200&family=Hind+Siliguri:wght@300&family=Montserrat:ital,wght@1,300&family=Roboto:wght@100&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Sistema Punto de ventas</title>
+</head>
+<body>
+    <?php require_once "../template/header.php"; ?>
+
+    <main class="main">
+        <h2>Hola</h2>
+    </main>
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.js" integrity="sha512-TsDUjQW16/G8fz4gmgTOBW2s2Oi6TPUtQ6/hm+TxZZdkQtQrK5xEFIE0rgDuz5Cl1xQU1u3Yer7K5IuuBeiCqw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+</html>

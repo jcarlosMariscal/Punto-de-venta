131c3e737a862c23a180adae8d8640de685636d1
Karl16Animelos20 <juanc.ramirezm16@gmail.com>
2022-05-17 21:34:32 -0500

    form user

--- view/personal.php
+++ view/personal.php
@@ -1,5 +1,7 @@
 <section class="above">
-    <div class="above__info">En desarrollo mi personal</div>
+    <div class="above__info">Mi Personal
+      <!-- <a href="index.php?p=compras" class="btn-prm btn-cancelar">Atrás</a> -->
+    </div>
     <div class="above__user">
         <div class="user__info">
             <p class="user__name">Carlos Mariscal</p>
@@ -12,72 +14,62 @@
 </section>
 <hr>
 
-
-<div class="card-group">
-    <div class="card-body">
-     <div class="above__info">Recuerde revisar los permisos de usuario en la configuracion</div>
-    </div>
-
-
-  <div class="row" class="card-body">
-  <span class="float-right"><button type="button" class=" btn btn-danger " data-toggle="modal" data-target=".bd-example-modal-lg">Agregar</button></span>
-  
-  </div>
-</div>
-
-<table table bgcolor= "#FFFFFF" class="table table-bordered">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Nombre</th>
-      <th scope="col">Correo</th>
-      <th scope="col">Telefono</th>
-      <th scope="col">Rol</th>
-      <th scope="col">Eliminar</th>
-      <th scope="col">Editar</th>
-      <th scope="col">Ver</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-      <td> </td>
-      <td> </td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <td>@fat</td>
-      <td> </td>
-      <td> </td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>@fat</td>
-      <td> </td>
-      <td> </td>
-    </tr>
-    <tr>
-      <th scope="row">4</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>@fat</td>
-      <td> </td>
-      <td> </td>
-    </tr>
-    
-  </tbody>
-</table>
+<section class="">
+    <section class="table-ver-product">
+        <div class="table-above">
+            <div class="product-filter">
+                <p>Recuerde que puede modificar los permisos en <a href="index.php?p=configuration">Configuración</a></p>
+            </div>
+            <div class="product-chart">
+                <a href="" class="btn-prm btn-cancelar" data-toggle="modal" data-target=".bd-example-modal-lg">Agregar</a>
+            </div>
+        </div>
+        <div class="table-ver">
+            <table table bgcolor= "#FFFFFF"  class="table table-bordered">
+                <thead>
+                  <tr>
+                      <th scope="col">#</th>
+                      <th scope="col">NOMBRE</th>
+                      <th scope="col">CORREO</th>
+                      <th scope="col">TELÉFONO</th>
+                      <th scope="col">ROL</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr class="prod">
+                      <td>1</td>
+                      <td>Carlos Mariscal</td>
+                      <td>prueba@gmail.com</td>
+                      <td>123456789</td>
+                      <td>Administrador</td>
+                      <td>Eliminar</td>
+                      <td>Editar</td>
+                      <td>Ver</td>
+                  </tr>
+                  <tr class="prod">
+                      <td>1</td>
+                      <td>Carlos Mariscal</td>
+                      <td>prueba@gmail.com</td>
+                      <td>123456789</td>
+                      <td>Administrador</td>
+                      <td>Eliminar</td>
+                      <td>Editar</td>
+                      <td>Ver</td>
+                  </tr>
+                  <tr class="prod">
+                      <td>1</td>
+                      <td>Carlos Mariscal</td>
+                      <td>prueba@gmail.com</td>
+                      <td>123456789</td>
+                      <td>Administrador</td>
+                      <td>Eliminar</td>
+                      <td>Editar</td>
+                      <td>Ver</td>
+                  </tr>
+                </tbody>
+            </table>
+        </div>
+    </section>
 
 <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-lg">
@@ -88,51 +80,47 @@
         </div>
         <div class="modal-body">
             <div class="permisos">
-                <form action="" class="">
-                    <div class="inputs">                                       
-                      <label for="">Nombre</label>
-                        <input type="text" name="" id="">
+                <form action="" class="form-user">
+                    <div class="input-user-name input-user">                                       
+                        <label for="">Nombre: </label>
+                        <input type="text" name="" id="" class="input" placeholder="Introduce un nombre">
                     </div>
-                    <div class="inputs">                                       
-                      <label for="">R.F.C.</label>
-                        <input type="text" name="" id="">
+                    <div class="input-user-rfc input-user">                                       
+                      <label for="">R.F.C.: </label>
+                        <input type="text" name="" id="" class="input" placeholder="Introduce">
                     </div>
-                    <div class="inputs">                                       
+                    <div class="input-user-fnac input-user">                                       
                       <label for="">Fecha de Nacimiento</label>
-                        <input type="text" name="" id="">
+                        <input type="date" name="" id="" class="input">
                     </div>
-                    <div class="inputs">                                       
+                    <div class="input-user-tel input-user">                                       
                       <label for="">Teléfono</label>
-                        <input type="text" name="" id="">
+                        <input type="number_format" name="" id="" class="input">
                     </div>
-                    <div class="inputs">                                       
+                    <div class="input-user-email input-user">                                       
                       <label for="">Correo</label>
-                        <input type="text" name="" id="">
+                        <input type="text" name="" id="" class="input">
                     </div>
-                    <div class="inputs">                                       
-                      <label for="">Rol</label>
-                      <select name="" id="">
+                    <div class="input-user-rol input-user">                                       
+                      <label for="">Seleccione el Rol:</label>
+                      <select name="" id="" class="select-user-rol">
                         <option value="">Administrador</option>
                         <option value="">Ventas</option>
                       </select>
                     </div>
-                    <div class="inputs">                                       
+                    <div class="input-user-caja input-user">                                       
                       <label for="">Caja</label>
-                        <input type="text" name="" id="">
+                        <input type="text" name="" id="" class="input">
                     </div>
-                    <div class="inputs">                                       
+                    <div class="input-user-password input-user">                                       
                       <label for="">Contraseña</label>
-                        <input type="text" name="" id="">
-                    </div>
-                    <div class="inputs">                                       
-                      <label for="">Repetir contraseña</label>
-                        <input type="text" name="" id="">
+                        <input type="text" name="" id="" class="input">
                     </div>
                   </form>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn-close-modal" data-dismiss="modal">Cerrar</button>
-                <button type="button" class="btn-save-modal">Guardar Cambios</button>
+                <button type="button" class="btn-save-modal">Agregar</button>
             </div>
         </div>
     </div>
